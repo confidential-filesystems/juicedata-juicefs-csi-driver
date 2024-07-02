d71294d262ce6e9114bfddcefcc17eb98ae97354
enuoCM <enuoCM@163.com>
2024-07-02 18:04:26 +0800

    update docker build

--- docker/cfs/build_docker.sh
+++ docker/cfs/build_docker.sh
@@ -2,10 +2,18 @@
 
 set -e
 SERVICE_NAME=juicefs-csi-driver
-VERSION=v0.0.1
+VERSION=v0.0.1-d1
 HUB=hub.confidentialfilesystems.com:4443
+
+git pull
+
+time=$(date "+%F %T")
+id=$(git rev-parse HEAD)
+GOOS=linux GOARCH=amd64 go build -o ${SERVICE_NAME} ../../cmd
 
 docker build -f ./Dockerfile -t ${HUB}/cc/${SERVICE_NAME}:${VERSION} .
 docker push ${HUB}/cc/${SERVICE_NAME}:${VERSION}
+
+rm -rf ${SERVICE_NAME}
 
 echo "build time: $(date)"
